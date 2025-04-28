206b09d2d8835b275ca6168fe41f91e7d49598a4
LuisLachapel <luislachapelcentropeguero@gmail.com>
2025-04-27 23:07:33 -0400

    Metodo de consulta de todos los usuarios funcional

--- Persistence/TypeRoom/Get.cs
+++ Persistence/TypeRoom/Get.cs
@@ -11,6 +11,49 @@ namespace Persistence.TypeRoom
 {
     public class Get: DBConnection
     {
+        public List<Entity.TypeRoom> List()
+        {
+            List<Entity.TypeRoom> typeRooms = new List<Entity.TypeRoom>();
+            using(SqlConnection connection = new SqlConnection(db_connection))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("uspListarTipoHabitacion", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        SqlDataReader reader = command.ExecuteReader();
+                        
+                        if (reader.HasRows)
+                        {
+                            
+                            int fieldId = reader.GetOrdinal("IIDTIPOHABILITACION");
+                            int fieldName = reader.GetOrdinal("NOMBRE");
+                            int fieldDescription = reader.GetOrdinal("DESCRIPCION");
 
+                            while (reader.Read())
+                            {
+                                Entity.TypeRoom typeRoom = typeRoom = new Entity.TypeRoom();
+                                typeRoom.id = reader.GetInt32(fieldId);
+                                typeRoom.name = reader.GetString(fieldName);
+                                typeRoom.description = reader.GetString(fieldDescription);
+                                typeRooms.Add(typeRoom);
+                            }
+                        }
+
+
+                    }
+                    connection.Close();
+                }
+                catch (Exception)
+                {
+
+                    connection.Close();
+                }
+
+            }
+            return typeRooms;
+
+        }
     }
 }
---
+++ Web/Controllers/TypeRoomController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Persistence.TypeRoom;
+
+namespace Web.Controllers
+{
+    public class TypeRoomController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult List()
+        {
+            Get list = new Get();
+            return Json(list.List());
+        }
+    }
+}
