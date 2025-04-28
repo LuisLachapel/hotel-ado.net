4c51529d26f5af1242b7ae1ba49953c96cf969ee
LuisLachapel <luislachapelcentropeguero@gmail.com>
2025-04-27 21:30:18 -0400

    Creacion de la clase TypeRoom

---
+++ Persistence/DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using DotNetEnv;
+
+namespace Persistence
+{
+    public class DBConnection
+    {
+        public string db_connection { get; set; }
+
+        public DBConnection()
+        {
+            Env.Load(@"D:\Cursos\C sharp\Proyecto Ado.Net\Proyecto Hotel\.env");
+            db_connection = Environment.GetEnvironmentVariable("db_connection");
+        }
+
+    }
+}
